d287f0f7f5465f2b55f78517d07bb01ef391993d
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-03-19 06:56:35 +0500

    chapter 05 question 4 Done

--- chapters/05/main.js
+++ chapters/05/main.js
@@ -15,13 +15,31 @@ let declareVariable = 6;
     //  b. Show the value of variable in your browser like "Value after variable declaration is : ??". 
     document.querySelector('#variable').innerHTML = `Value after variable declaration is : ${declareVariable}`
     //    c. Initialize the variable with some number. 
+           let anyNumber = 10 ;
     //    d. Show the value of variable in your browser like "Initial value : 5". 
-    //    e. Increment the variable. f. Show the value of variable in your browser like "Value after increment is : 6". g. Add 7 to the variable. h. Show the value of variable in your browser like "Value after addition is : 13". 
-    //   i. Decrement the variable. j. Show the value of variable in your browser like "Value after decrement is : 12". 
-    //    k. Show the remainder after dividing the variable's value by 3. 1. Output : "The remainder is : 0". tin%
+    document.querySelector("#showNumber1").innerHTML = `Initial value is : ${anyNumber}`
+    //    e. Increment the variable. 
+    anyNumber++
+    // f. Show the value of variable in your browser like "Value after increment is : 6". 
+    document.querySelector("#showNumber2").innerHTML = `Value after increment is :${anyNumber}`
+    // g. Add 7 to the variable. 
+    anyNumber += 7
+    // h. Show the value of variable in your browser like "Value after addition is : 13". 
+    document.querySelector("#showNumber3").innerHTML = `Value after addition 7 is : ${anyNumber}`
 
+    //   i. Decrement the variable.
+    anyNumber--
+    //  j. Show the value of variable in your browser like "Value after decrement is : 12". 
+    document.querySelector("#showNumber4").innerHTML = `Value after decrement is : ${anyNumber}`
+    
+    //    k. Show the remainder after dividing the variable's value by 3. 1. Output : "The remainder is : 0". tin%
+    document.querySelector("#showNumber5").innerHTML = `The remainder is : ${anyNumber % 3}`
+    
 
     // 4. Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie. Example output :
+    let movieTicket = 600
+    let buyTicket = 5
+    document.querySelector("#movei").innerHTML = `Total Cost to buy ${buyTicket} to a movie ${movieTicket * buyTicket}PKR`
     // 5. Write a script to display multiplication table of any number in your browser. E.g
 
     // 6. The Temperature Converter : It's hot out! Let's make a converter based on the steps here. a. Store a Celsius temperature into a variable. b. Convert it to Fahrenheit & output "NNoC is NNoF". c. Now store a Fahrenheit temperature into a variable. d. Convert it to Celsius & output "NNoF is NN.C". Conversion 
