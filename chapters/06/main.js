c28e07d348f812815d3094ccad6288f9381acff7
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-03-21 19:50:29 +0500

    all question written

---
+++ chapters/06/main.js
@@ -0,0 +1,21 @@
+// 1. Write a program to take a number in a variable, do the required arithmetic to display the following result in your browser :
+
+
+// M 2. What will be the output in variables a, b & result after execution of the following script
+//  : var a = 2, b = 1; var result = --a---b+++b+ b--; 
+// Explain the output at each stage :
+//  --a; 
+//  --a---b;
+//   --a - --b + ++b;
+//    --a --b+++b+ b--;
+
+
+// 3. Write a program that takes input a name from user & greet the user.
+//  4. Write a program to take input a number from user & display it's multiplication table on your browser. 
+//  If user does not enter a new number, multiplication table of 5 should be displayed by default.
+//  6. Take 
+// a) Take three subjects name from user and store them in 3 different variables.
+//  b) Total marks for each subject is 100, store it in another variable. 
+// c) Take obtained marks for first subject from user and stored it in different variable.
+// d) Take obtained marks for remaining 2 subjects from user and store them in variables.
+//  e) Now calculate total marks and percentage and show the result in browser like this. (Hint : user table)
