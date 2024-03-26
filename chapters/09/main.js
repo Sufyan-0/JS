3241f30c9fc63578889df77eb389f5a07ab1080f
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-03-26 07:24:47 +0500

    questions pasted

---
+++ chapters/09/main.js
@@ -0,0 +1,27 @@
+// 1. Declare an empty array using JS literal notation to store student names in future.
+//  2. Declare an empty array using JS object notation to store student names in future.
+//  3. Declare and initialize a strings array.
+//  4. Declare and initialize a numbers array.
+//  5. Declare and initialize a boolean array. 
+// 6. Declare and initialize a mixed array. 
+
+//  7. Declare and Initialize an array and store available education qualifications in Pakistan
+//  (e.g. SSC, HSC, BCS, BS, BCOM, MS, M. Phil., PhD). Show the listed qualifications in your browser like :
+
+// 8. Write a program to store 3 student names in an array.Take another array to store score of these three students.
+//  Assume that total marks are 500 for each student, display the scores & percentages of students like :
+
+// 9. Initialize an array with color names. 
+// Display the array elements in your browser. 
+// a. Ask the user what color he/she wants to add to the beginning & add that color to the beginning of the array.
+//  Display the updated array in your browser. 
+//  b. Ask the user what color he/she wants to add to the end & add that color to the end of the array.
+//  Display the updated array in your browser.
+//  c. Add two more color to the beginning of the array. 
+// Display the updated array in your browser.
+//  d. Delete the first color in the array.
+//  Display the updated array in your browser. 
+// e. Delete the last color in the array.
+// Display the updated array in your browser. 
+// f. Ask the user at which index he/she wants to add a color & color name. 
+// Then add the color to desired
