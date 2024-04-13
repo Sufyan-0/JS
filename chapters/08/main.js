fe91519e19b562c7a045e6b09a2d30b3a0aa3f6c
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-04-13 07:52:49 +0500

    few question remain

--- chapters/08/main.js
+++ chapters/08/main.js
@@ -6,7 +6,15 @@
 
 // 3. Write a program that takes input a number from user & state whether the number is positive, 
 // negative or zero. 
+let yourNumber = 5;
 
+if (yourNumber > 0) {
+  console.log(yourNumber + " is a positive number.");
+} else if (yourNumber === 0) {
+  console.log(yourNumber + " is zero.");
+} else {
+  console.log(yourNumber + " is a negative number.");
+}
 // 4. Write a program that takes a character (i.e. string of length 1) and returns true if it is a vowel,
 //  false otherwise
 
@@ -21,6 +29,17 @@
 
 // 6. This if/else statement does not work. Try to fix it : var greeting;
 //  var hour = 13; if (hour <18) { greeting = "Good day"; else greeting = "Good evening"; } puting 
+
+var greeting;
+var hour = 13;
+
+if (hour < 18) { 
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+
+console.log(greeting);
 
 // 7. Write a program that takes time as input from user in 24 hours clock format like : 1900 = 7pm.
 //  Implement the following case using if, else & else if statements
