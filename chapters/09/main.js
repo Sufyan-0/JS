f837c59a784ebfda3930e91e75711dcac81000fa
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-04-15 16:56:57 +0500

    9 10 11 and 13 Q remaining

--- chapters/09/main.js
+++ chapters/09/main.js
@@ -1,39 +1,52 @@
-// 1. Declare an empty array using JS literal notation to store student names in future.
-const student_names = [];
-//  2. Declare an empty array using JS object notation to store student names in future.
-const student_names_object = {};
-//  3. Declare and initialize a strings array.
-const fruit = ["apple", "banana", "orange"];
-//  4. Declare and initialize a numbers array.
-const NumberArray = [85, 92, 78];
-//  5. Declare and initialize a boolean array. 
-const booleanArray = [true, false, true];
-// 6. Declare and initialize a mixed array. 
-const MixsArray = ["Sufyan", 19, true, "developer"];
+// // 1. Declare an empty array using JS literal notation to store student names in future.
+// const student_names = [];
+// //  2. Declare an empty array using JS object notation to store student names in future.
+// const student_names_object = {};
+// //  3. Declare and initialize a strings array.
+// const fruit = ["apple", "banana", "orange"];
+// //  4. Declare and initialize a numbers array.
+// const NumberArray = [85, 92, 78];
+// //  5. Declare and initialize a boolean array.
+// const booleanArray = [true, false, true];
+// // 6. Declare and initialize a mixed array.
+// const MixsArray = ["Sufyan", 19, true, "developer"];
 
-//  7. Declare and Initialize an array and store available education qualifications in Pakistan
-//  (e.g. SSC, HSC, BCS, BS, BCOM, MS, M. Phil., PhD). Show the listed qualifications in your browser like :
-const educationQualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil.", "PhD"];
+// //  7. Declare and Initialize an array and store available education qualifications in Pakistan
+// //  (e.g. SSC, HSC, BCS, BS, BCOM, MS, M. Phil., PhD). Show the listed qualifications in your browser like :
+// const educationQualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil.", "PhD"];
 
-console.log("Available Qualifications:", educationQualifications);
+// console.log("Available Qualifications:", educationQualifications);
 
 // 8. Write a program to store 3 student names in an array.Take another array to store score of these three students.
 //  Assume that total marks are 500 for each student, display the scores & percentages of students like :
-
-// 9. Initialize an array with color names. 
-// Display the array elements in your browser. 
+    // let arrNames = ["Sufyan", "Abdullah", "Rizwan"];
+    // let arrNumber = [390, 400, 480];
+    // let totalNumber = 500
+    // for (let i = 0; i < arrNames.length; ) {
+    // for (let j = 0; j < arrNumber.length; ) {
+    //     console.log(
+    //     `${arrNames[i]} score is ${arrNumber[j]} and percentage is ${
+    //         (arrNumber[j] / totalNumber) * 100
+    //     }%`
+    //     );
+    //     i++;
+    //     j++;
+    // }
+    // }
+// 9. Initialize an array with color names.
+// Display the array elements in your browser.
 // a. Ask the user what color he/she wants to add to the beginning & add that color to the beginning of the array.
-//  Display the updated array in your browser. 
+//  Display the updated array in your browser.
 //  b. Ask the user what color he/she wants to add to the end & add that color to the end of the array.
 //  Display the updated array in your browser.
-//  c. Add two more color to the beginning of the array. 
+//  c. Add two more color to the beginning of the array.
 // Display the updated array in your browser.
 //  d. Delete the first color in the array.
-//  Display the updated array in your browser. 
+//  Display the updated array in your browser.
 // e. Delete the last color in the array.
-// Display the updated array in your browser. 
-// f. Ask the user at which index he/she wants to add a color & color name. 
-// Then add the color to desired position/index.. Display the updated array in your browser. 
+// Display the updated array in your browser.
+// f. Ask the user at which index he/she wants to add a color & color name.
+// Then add the color to desired position/index.. Display the updated array in your browser.
 // g. Ask the user at which index he/she wants to delete color(s) & how many colors he/she wants to delete. Then
 // remove the same number of color(s) from user-defined position/index.. Display the updated array in your browser.
 
@@ -41,12 +54,12 @@ console.log("Available Qualifications:", educationQualifications);
 
 // 11. Write a program to initialize an array with city names. Copy 3 array elements from cities array to selected Cities array.
 
-// 12. Write a program to create a single string from the below mentioned 
+// 12. Write a program to create a single string from the below mentioned
 // array : 66 var arr = ["This ", " is ", " my ", " cat”]; (Use array's join method)
-const words = ["This ", " is ", " my ", " cat"];
-const sentence = words.join("");
+// const words = ["This ", " is ", " my ", " cat"];
+// const sentence = words.join("");
 
-console.log(sentence);
+// console.log(sentence);
 
 // 13. Create a new array. Store values one by one in such a way that t you can access the values in the order in which they were stored.
 //  (FIFO-First In First Out)
