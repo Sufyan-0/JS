2ccf4d42161f5d2ea524e5ed0bda1d0d7f15dfaa
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-04-15 16:00:47 +0500

    chapter 8 ~ Done

--- chapters/08/main.js
+++ chapters/08/main.js
@@ -3,18 +3,27 @@
 
 // 2. Write a JavaScript program that accept two integers and display the larger. 
 // Also show if the two integers are equal. 
+    // let int1 = +prompt("Enter First Integer")
+    // let int2 = +prompt("Enter Second Integeer")
+    // if (int1 > int2 ){
+    //   alert(`first integer is greater ${int1}`)
+    // }else if (int1<int2){
+    //   alert(`Second Integer is greater ${int2}`)
+    // }else{
+    //   alert(`Both are equal ${int1} = ${int2}`)
+    // }
 
 // 3. Write a program that takes input a number from user & state whether the number is positive, 
 // negative or zero. 
-let yourNumber = 5;
+// let yourNumber = 5;
 
-if (yourNumber > 0) {
-  console.log(yourNumber + " is a positive number.");
-} else if (yourNumber === 0) {
-  console.log(yourNumber + " is zero.");
-} else {
-  console.log(yourNumber + " is a negative number.");
-}
+// if (yourNumber > 0) {
+//   console.log(yourNumber + " is a positive number.");
+// } else if (yourNumber === 0) {
+//   console.log(yourNumber + " is zero.");
+// } else {
+//   console.log(yourNumber + " is a negative number.");
+// }
 // 4. Write a program that takes a character (i.e. string of length 1) and returns true if it is a vowel,
 //  false otherwise
 
@@ -26,20 +35,29 @@ if (yourNumber > 0) {
 //  then give message "Please enter your password" Mobi Check if both passwords are same. 
 // If they are same, show message "Correct! The password you entered matches the original password". 
 // Show "Incorrect password" otherwise.
+    // let password = "sufyan123"
+    // let userEnterPasswad  = prompt("Enter your Passward")
+    // if (userEnterPasswad === ""){
+    //   alert("Enter passward")
+    // }else if (userEnterPasswad === password){
+    //   alert("correct")
+    // }else {
+    //   alert("Incorrect")
+    // }
 
 // 6. This if/else statement does not work. Try to fix it : var greeting;
 //  var hour = 13; if (hour <18) { greeting = "Good day"; else greeting = "Good evening"; } puting 
 
-var greeting;
-var hour = 13;
+// var greeting;
+// var hour = 13;
 
-if (hour < 18) { 
-  greeting = "Good day";
-} else {
-  greeting = "Good evening";
-}
+// if (hour < 18) { 
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
 
-console.log(greeting);
+// console.log(greeting);
 
-// 7. Write a program that takes time as input from user in 24 hours clock format like : 1900 = 7pm.
-//  Implement the following case using if, else & else if statements
+// // 7. Write a program that takes time as input from user in 24 hours clock format like : 1900 = 7pm.
+// //  Implement the following case using if, else & else if statements
