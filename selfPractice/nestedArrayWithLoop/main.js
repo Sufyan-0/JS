26fe724e89d403fe6834fc57daffc0e3d3a89516
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-04-18 08:29:33 +0500

    nested arry print all value dynamiclly Done

---
+++ selfPractice/nestedArrayWithLoop/main.js
@@ -0,0 +1,6 @@
+let myarr = [[2,3,4,5,6,7,7], [2,5], [9,43,6,7]]
+for(let i = 0 ; i <myarr.length; i ++){
+    for (let j = 0 ; j <myarr[i].length ; j++){
+        console.log(myarr[i][j])
+    }
+}
