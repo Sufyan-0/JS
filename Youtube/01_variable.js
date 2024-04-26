47b1e42b60cce27f3525acf97948aa0cdde5348e
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-04-26 12:29:08 +0500

    varibales Done

---
+++ Youtube/01_variable.js
@@ -0,0 +1,17 @@
+const productID = 121212;
+let productName = "chiliMili"
+var price = 12
+description = "amazing" // this is also not recommended
+
+
+// productID = 1212 not allowed we can not change constant value 
+
+/*
+prefer not to use var keyword for variable because it has some scope issue 
+*/
+/*
+now a days people are just using let and const keywords  for variables
+*/
+
+console.table([productID , productName , price , description])
+
