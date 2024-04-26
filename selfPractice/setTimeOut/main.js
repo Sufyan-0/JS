48164104646c75b754f3cd56c99f56de2577cda4
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-04-26 12:00:04 +0500

    set time out concept done

---
+++ selfPractice/setTimeOut/main.js
@@ -0,0 +1,22 @@
+// for (let i = 0 ; i < 5 ; i++ ){
+//     // console.log(i)    // 0 1 2 3 4 
+//     setTimeout(()=> console.log(i) , 2000)  // 0 1 2 3 4 
+// }
+// let counter = 0 
+
+for (var j = 0 ; j <5 ; j++){
+    // console.log(i)  // 0 1 2 3 4
+    // console.log("upper")
+    // setTimeout(()=> console.log(i , counter++) , 2000)  // 1
+    // console.log("lower");
+    // console.log(i)
+}
+
+console.log(j)
+
+
+
+// second loop var 
+
+// settimeout sy uper or nichy chala leta hai but settimeout 2 second bad chaly gha or akhata but ya is ko count rakhy gha 
+// ky kitny dafa pending ma hai ya line or phr ya khati execution karway gha jo i ki value ho ghi last ma
