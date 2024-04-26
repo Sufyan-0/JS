3bbb70d65e2739a336542830e2f08ec9d68c33ea
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-04-26 12:52:50 +0500

    datatypes

---
+++ Youtube/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as a newer version
+
+// alert(3 + 3) // this is node JS not a browser
+
+console.log(3 + 3); // recommend and readable
+
+console.log(3 
+    + 3
+);  // not recommend and unreadable 
+
+
+let age = 20 // Number
+let name = "sufyan" //string
+let isLogged = false // bolean
+
+// 1.Number => 2 to power 53 
+    //  BigInt
+// 2.string => "" , '' , ``
+// 3.bolean =>true/false
+// 4.null => data type but it is standAlone Value  and its type is object
+// 5. undefined => variable is declare but value is empty its type is undefined
+// 6. symbol => unique
+// 7. object 
+
+
+
