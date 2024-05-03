21e37eca2f16c94e3dab4e26727738997549151c
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-05-03 09:06:54 +0500

    basic conversion is Done

---
+++ Youtube/03_conversionOperation.js
@@ -0,0 +1,14 @@
+// conversion
+
+let number = String(33);
+console.log(typeof number)  //string
+console.log(number);
+
+let string = Number("Sufyan");
+console.log(typeof string)  // Number
+console.log(string); //NAN
+
+let bolean = Boolean(34)
+console.log(bolean) //true 
+
+// in bolean truthy and falsy method is using 
