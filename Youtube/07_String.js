10388ec5f166070ac78bfa9196176786b9473c63
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-05-14 15:56:23 +0500

    string methods

---
+++ Youtube/07_String.js
@@ -0,0 +1,38 @@
+let name = "Sufyan";
+let age = 20 ;
+// console.log(`My name is ${name} and I am ${age} years Old.`)
+
+let username = new String("Sufyan")
+// console.log(username)
+
+
+let gameName = new String("FreeFire")
+console.log(gameName.at(-2))  // it take index number(positive and Negative) and return index value
+
+console.log(gameName.charAt(2)); // it takes just positive index number and return value 
+
+console.log(gameName.charCodeAt(5)); // it will return ASCII Code of letter
+
+console.log(gameName.codePointAt(5)) // it is same as charcode but have some deffrence
+
+console.log("     sufyan      ".trim())
+
+console.log(gameName.includes("e")) // it will check this value is in my varibale or not if yes return true if not return false
+
+
+// console.log(gameName.charAt.matchAll())
+
+console.log(gameName.repeat(2))  // repeat that variable means print multiple time
+
+
+// replace
+// replaceALL
+// search
+//slice
+//small
+// split 
+//startswith
+//sub
+//substr
+//substring
+//value of 
