92a607ea2af70b73dedb51ab6ecc34b2ad18a3c7
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-05-18 00:19:10 +0500

    basic math obj

---
+++ Youtube/09_Math.js
@@ -0,0 +1,9 @@
+// console.log(Math);
+
+console.log(Math.abs(-6)) // it will change negative sign to positive
+
+console.log(Math.round(5.6));// it will do round of same as school
+
+console.log(Math.ceil(3.3)); // it will plus one value if it has value before point
+
+console.log(Math.floor(3.3)); // it will removes values after point
