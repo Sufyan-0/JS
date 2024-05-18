6440eee58cd843ebbd74cdb197c349dc31713496
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-05-18 14:35:46 +0500

    Math basic Done

--- Youtube/09_Math.js
+++ Youtube/09_Math.js
@@ -7,3 +7,13 @@ console.log(Math.round(5.6));// it will do round of same as school
 console.log(Math.ceil(3.3)); // it will plus one value if it has value before point
 
 console.log(Math.floor(3.3)); // it will removes values after point
+
+// mostly we use math.random()
+
+console.log(Math.random())
+
+// Math.random () will give value in points and value start form 0.0 to 0.9 
+
+let min = 10 
+let max = 20
+console.log(Math.floor(Math.random()*(max - min + 1) + min))
