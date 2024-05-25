d24b2311be63b84066d17051ad6dd7bf862f69e3
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-05-25 15:03:19 +0500

    play with time and de-structure

---
+++ selfPractice/typeOf/types.js
@@ -0,0 +1,22 @@
+let undtype_ = typeof undefined
+
+console.log(typeof undefined); //
+console.log(typeof undtype_); // 
+
+
+let nulltype_ = typeof null
+
+console.log(typeof null); //
+console.log(typeof nulltype_); //
+
+// let fName = "Sufyan"
+// let LName = "khan"
+// console.log("Checking " , fName, LName)
+
+// let name = {fName , LName} = ""
+// console.log("check2", typeof name);
+
+// console.log( "check" , name)
+
+// let CheckingName = ""
+// console.log("cjecl" , CheckingName);
