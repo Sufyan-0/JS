15a9561511f83cdba4eefd3978e283fc072527dd
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-05-28 14:38:43 +0500

    destructure and json info

---
+++ Youtube/13_deStructureAndJson.js
@@ -0,0 +1,24 @@
+const obj = {
+    name : "sufyan",
+    age : 29,
+    info : "data info"
+}
+
+// let {info } = obj;
+// console.log(info);
+
+let {name : na, age : ag , info : inf } = obj
+// console.log(name)
+// console.log(obj.name);
+// console.log(na); now i can use name varibale as a na 
+
+// json Syntax 
+
+// {
+//     "name" : "sufyan",
+//     "age" : 20,
+//     "price" : "10k"
+
+// }
+
+// json => api = api ka data object ma bhi ho sakta or array ma bhi
