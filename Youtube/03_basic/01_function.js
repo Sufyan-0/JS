c1c3f594ecae409df9f12ee412b592271b4f82c6
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-05-28 15:18:23 +0500

    function basic , argument , parameter , default parameter

--- Youtube/03_basic/01_function.js
+++ Youtube/03_basic/01_function.js
@@ -12,3 +12,24 @@ function funcName2 (){
 }
 let result1 = funcName2 ()
 console.log(result1) // 5
+
+// parameter and arguments
+// parameter when we decalare function and set parameter value 
+function myFunc (num1 , num2){
+    return num1 + num2
+}
+
+console.log(myFunc()) //NAN
+// argument when we call function 
+console.log(myFunc(2, 3)) // 5
+
+
+
+// defult parameter in function 
+
+
+//  what we set default in parameter it will show when i have no argument but if i have argument it will overwrite
+function myFunc1 (userName = "user"){
+   return `${userName} just Logged in`
+}
+console.log(myFunc1("Sufyan")) 
