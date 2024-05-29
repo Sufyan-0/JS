6df61295b4a25ddaecb66815de09cdd1e10108d6
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-05-29 20:52:37 +0500

    nested scope

--- Youtube/03_basic/02_scope.js
+++ Youtube/03_basic/02_scope.js
@@ -15,3 +15,36 @@ if(true){
 }
 console.log(name); // it will return sufyan
 console.log(innerValue) // it will return 59 
+
+// +++++++++++
+
+// function in function scope || nested scope 
+function one (){
+    let userName  = "sufyan"
+    function two (){
+        const website = " Instagaram"
+        console.log(userName + website) // sufyan Instagaram
+    }
+    // console.log(website)  // not define
+    two()
+}
+one()
+
+
+// hoisting and two types of declaration of function 
+
+console.log(func1(6)) // 7 because func1 is global
+
+function func1 (num){
+    return  num + 1 
+}
+
+console.log(func1(3)) // 4
+
+console.log(addTwo(6))  // Cannot access 'addTwo' before initialization 
+
+const addTwo = function (num){
+    return num+2
+}
+
+console.log(addTwo(3)) // 5
