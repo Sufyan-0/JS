052910aaef61f0f2dd2905e7f235ecafde0119c3
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-05-30 15:03:07 +0500

    this keyword

--- Youtube/03_basic/03_arrowAndThis.js
+++ Youtube/03_basic/03_arrowAndThis.js
@@ -5,9 +5,28 @@ let user = {
         console.log(`${this.name} , thanks for visiting our web page`)
     } 
 }
+// this just work in object not in function below you can see
 
 user.message()
 user.name = "Sufi"
 user.message()
 
 console.log(this) // {}
+
+function chai (){
+    let userName = "Sufyan"
+    console.log(this.userName)  // undefined
+}
+chai()
+
+
+function chai2 (){
+    console.log(this) // this will return big object it is like global this  
+}
+chai2()
+
+const chai = ()=>{
+   console.log(this) // this will return {} and
+}
+
+chai()
