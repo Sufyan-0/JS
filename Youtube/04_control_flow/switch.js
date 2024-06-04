ce84dd6495a2dde21482ccc4d32e7a0b67c111bd
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-06-04 14:12:08 +0500

    switch case done

---
+++ Youtube/04_control_flow/switch.js
@@ -0,0 +1,18 @@
+switch (9) {
+    case 5:
+        console.log("5 mil gata")
+        
+        break;
+    case 7:
+        console.log("7 mil gata")
+        
+        break;
+    case 9:
+        console.log("9 mil gata")
+        
+        break;
+
+    default:
+        console.log("dont FInd")
+        break;
+}
