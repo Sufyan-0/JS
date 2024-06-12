c7e2e8afece887f14e7cf9356c7f6f3e24cfd4e4
Sufyan Ahmed Dev <sufiyan033130@gmail.com>
2024-06-12 15:26:11 +0500

    while and do while loop done

---
+++ Youtube/05_loops/02_whileLoop.js
@@ -0,0 +1,15 @@
+
+// let index = 0 
+// while (index < 10) {
+//     console.log(`index number is ${index}`)
+//     index++
+    
+// }
+
+// do while 
+// aik dafa lazmi chaly gha 
+let num = 0 
+do{
+    console.log(`I am Sufyan ${num}`)
+    num++
+}while(num< 10)
